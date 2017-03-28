f7771658954a6cc227300f81b6fcbf909516823b
ShinByongRyun <nj298@kumoh.ac.kr>
2017-03-28 11:41:05 +0900

    mod

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,36 +1,46 @@
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
+import javax.swing.ButtonGroup;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JToggleButton;
 
-public class MainFrame extends JFrame{
-	JButton[] b=new JButton[5];
+public class MainFrame extends JFrame implements ActionListener{
+	JToggleButton[] b=new JToggleButton[5];
 	String[] str={"배차차량조회", "예약신청", " 차량좌석조회", "승차권발매", "예약조회"};
 	CardLayout cards=new CardLayout();
+	JPanel cardpanel;
+	ButtonGroup bg;
 	public MainFrame(String userid){
-		setSize(700, 700);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setTitle("고속버스예매 프로그램");
 		
 		JPanel p=new JPanel(new BorderLayout());
 		JPanel p1=new JPanel();
-		JPanel p2=new JPanel();
+		JPanel p2=new JPanel(new FlowLayout(FlowLayout.CENTER, 20, 5));
 		JPanel p3=new JPanel(new BorderLayout());
 		JPanel tp=new JPanel();
-		JPanel cardpanel=new JPanel();
+		cardpanel=new JPanel(cards);
 		tp.setBackground(Color.black);
 		
 		JLabel title=new JLabel("고속버스예매 프로그램");
-		title.setFont(new Font("", Font.BOLD, 30));
+		title.setFont(new Font("", Font.BOLD, 25));
 		p1.add(title);
 		
+		bg=new ButtonGroup();
+		
 		for(int i=0; i<5; i++){
-			b[i]=new JButton(str[i]);
+			b[i]=new JToggleButton(str[i]);
+			b[i].addActionListener(this);
+			bg.add(b[i]);
 			p2.add(b[i]);
 		}
 		
@@ -38,17 +48,34 @@ public class MainFrame extends JFrame{
 		p3.add(tp, BorderLayout.CENTER);
 		
 		SelectBus sb=new SelectBus();
-		cardpanel.add(sb);
+		cardpanel.add(sb, "selectbus");
 		
-		//cards.addLayoutComponent("selectbus", sb);
-		//cards.show(cardpanel, "selectbus");
+		cardpanel.setPreferredSize(new Dimension(800, 550));
 		
 		p.add(p1, BorderLayout.NORTH);
 		p.add(p3, BorderLayout.CENTER);
-		sb.add(tp, BorderLayout.NORTH);
 		p.add(cardpanel, BorderLayout.SOUTH);
 		add(p);
+		pack();
 		setResizable(true);
 		setVisible(true);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource()==b[0]){
+			cards.show(cardpanel, "selectbus");
+		}
+		else if(e.getSource()==b[1]){
+		}
+		else if(e.getSource()==b[2]){
+			
+		}
+		else if(e.getSource()==b[3]){
+			
+		}
+		else if(e.getSource()==b[4]){
+			
+		}
 	}
 }
